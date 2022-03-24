80014c74df847ad90693c799f6f3d7e648b5d8dd
sujin-create <qsoojinq@naver.com>
2022-03-25 04:27:40 +0900

    ADD : signup form page 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./components/signin";
+import Signup from "./components/signup";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/Signin" element={<Signin />} />
+          <Route path="/Signup" element={<Signup />} />
          
         </Routes>
       </BrowserRouter>
